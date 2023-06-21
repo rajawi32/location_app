0390eb8106c8bd8ea8221362a0f41760d6e1d918
abderrahim boujedar <abderrahimboujedar@gmail.com>
2023-06-21 16:50:07 +0100

    locataire model

--- laravel_location_app/app/Models/Locataire.php
+++ laravel_location_app/app/Models/Locataire.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Proprietaire extends Model
+class Locataire extends Model
 {
     use HasFactory;
 }
